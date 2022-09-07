f09ec0f8d09a70716fcd44319cbd8659150de63e
zqy <850074816@qq.com>
2022-09-07 13:37:32 +0800

    update new_map2

--- std_lib/map_test/main2.go
+++ std_lib/map_test/main2.go
@@ -11,22 +11,23 @@ func new_map1() map[string]int {
 
     m["k1"] = 1
 
-    fmt.Printf("m: %p\n", &m)
+    fmt.Printf("new_map1, m: %p\n", &m)
     return m
 }
 
 
 func new_map2() map[string]int {
 
-    n := map[string]int{"foo": 1, "bar": 2}
+    m := map[string]int{"foo": 1, "bar": 2}
 
-    return n
+    fmt.Printf("new_map2: m: %p\n", &m)
+    return m
 }
 
 
 func display_map1(m map[string]int) {
 
-    fmt.Printf("m: %p\n", &m)
+    fmt.Printf("display_map1, m: %p\n", &m)
 
     for key, val := range m {
 
@@ -39,7 +40,7 @@ func display_map1(m map[string]int) {
 
 func display_map2(m *map[string]int) {
 
-    fmt.Printf("m: %p\n", m)
+    fmt.Printf("display_map2, m: %p\n", m)
 
     for key, val := range *m {
 
@@ -56,9 +57,16 @@ func main() {
 
     m1 := new_map1()
     fmt.Printf("m1: %p\n", &m1)
-
     display_map1(m1)
     display_map2(&m1)
+    fmt.Println()
+
+
+    m2 := new_map2()
+    fmt.Printf("m2: %p\n", &m2)
+    display_map1(m2)
+    display_map2(&m2)
+    
 
 
 
